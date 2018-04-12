2e225b7a7b979a3871baf5c6c7dc53ce67a4fb2d
rjflavin <rjflavin@gmail.com>
2018-04-11 18:16:36 -0700

    Add files via upload
    Initial Upload

---
+++ ShapeGenerator/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ShapeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.changeColorButton = new System.Windows.Forms.Button();
+            this.toggleColorCheckBox = new System.Windows.Forms.CheckBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1256, 969);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // changeColorButton
+            // 
+            this.changeColorButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.changeColorButton.Location = new System.Drawing.Point(1282, 12);
+            this.changeColorButton.Name = "changeColorButton";
+            this.changeColorButton.Size = new System.Drawing.Size(535, 102);
+            this.changeColorButton.TabIndex = 2;
+            this.changeColorButton.Text = "Change Color (C)";
+            this.changeColorButton.UseVisualStyleBackColor = true;
+            this.changeColorButton.Click += new System.EventHandler(this.changeColorButton_Click);
+            // 
+            // toggleColorCheckBox
+            // 
+            this.toggleColorCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.toggleColorCheckBox.AutoSize = true;
+            this.toggleColorCheckBox.Location = new System.Drawing.Point(1453, 301);
+            this.toggleColorCheckBox.Name = "toggleColorCheckBox";
+            this.toggleColorCheckBox.Size = new System.Drawing.Size(258, 36);
+            this.toggleColorCheckBox.TabIndex = 3;
+            this.toggleColorCheckBox.Text = "Toggle Color (T)";
+            this.toggleColorCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetButton.Location = new System.Drawing.Point(1282, 120);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(535, 102);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset (R)";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.startStopButton.Location = new System.Drawing.Point(1274, 776);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(543, 102);
+            this.startStopButton.TabIndex = 6;
+            this.startStopButton.Text = "Stop (Space)";
+            this.startStopButton.UseVisualStyleBackColor = true;
+            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1274, 884);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(543, 97);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Export Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1829, 993);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startStopButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.toggleColorCheckBox);
+            this.Controls.Add(this.changeColorButton);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "ShapeShifter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button changeColorButton;
+        private System.Windows.Forms.CheckBox toggleColorCheckBox;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button startStopButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ShapeGenerator/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ShapeGenerator
+{
+    public partial class Form1 : Form
+    {
+        int time = 1;
+        int shapeWidth = Screen.PrimaryScreen.WorkingArea.Width;
+        int shapeHeight = Screen.PrimaryScreen.WorkingArea.Height;
+        int xShift = 0;
+        int yShift = 0;
+        int xStart = 0;
+        int yStart = 0;
+        Timer timer = new Timer();
+        //int penColor = 0;
+        public static Color penColor { get; set; }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int random = Math.Abs((int)(DateTime.Now.Ticks) % 255);
+            penColor = Color.FromArgb(random, (random * random) % 255, (int)(random * random * random) % 255);
+
+            pictureBox1.Size = new Size(Screen.PrimaryScreen.WorkingArea.Width - changeColorButton.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            pictureBox1.Image = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width-changeColorButton.Width, Screen.PrimaryScreen.WorkingArea.Height);
+
+            xStart = pictureBox1.Width / 2;
+            yStart = pictureBox1.Width / 2;
+
+            startTimer();
+        }
+
+        public void startTimer()
+        {
+          
+
+            
+            timer.Interval = 10;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //grab previous bitmap
+            pictureBox1.Image = (Bitmap)pictureBox1.Image.Clone();
+
+            //Toggle Color if checked
+            if(toggleColorCheckBox.CheckState==CheckState.Checked)
+            {
+                changeColorButton_Click(null, null);
+            }
+
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            Pen pen = new Pen(penColor,1);
+            int circleWidth = time % shapeWidth + 1;
+            int circleHeight = time % shapeWidth + 1;
+            g.DrawArc(pen, new Rectangle(xShift+xStart-circleWidth/2,yStart+yShift-circleHeight/2, circleWidth, circleHeight), 0, 360);
+            
+            time += 3;
+        }
+
+        private void moveUpButton_Click(object sender, EventArgs e)
+        {
+            yShift += 10;
+        }
+
+        private void changeColorButton_Click(object sender, EventArgs e)
+        {
+
+            int random = Math.Abs((int)(DateTime.Now.Ticks) % 255);
+
+
+            penColor = Color.FromArgb(random, (random*random)%255, (int)(random*random*random)%255);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int key = e.KeyValue;
+            Console.WriteLine(key.ToString());
+
+            if (key==32)
+            {
+                startStopButton.PerformClick();
+            }
+            if (key == 67)
+            {
+                changeColorButton.PerformClick();
+            }
+            if(key==84)
+            {
+                // everytime you click the button checkbox states will change.
+                if (toggleColorCheckBox.Checked)
+                {
+                    toggleColorCheckBox.Checked = false;
+                }
+                else
+                {
+                    toggleColorCheckBox.Checked = true;
+                }
+            }
+            if(key==82)
+            {
+                resetButton.PerformClick();
+            }
+            if(key==37)
+            {
+                xShift -= 10;
+            }
+            if(key==39)
+            {
+                xShift += 10;
+            }
+            if(key==38)
+            {
+                yShift -= 10;
+            }
+            if(key==40)
+            {
+                yShift += 10;
+            }
+            if(key==81)
+            {
+                time = 1;
+                time = 1;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void moveDownButton_Click(object sender, EventArgs e)
+        {
+            yShift -= 10;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //MessageBox.Show("X: "+e.X+",Y:"+e.Y);
+
+            xStart = e.X;
+            yStart = e.Y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width - changeColorButton.Width, Screen.PrimaryScreen.WorkingArea.Height);
+        }
+
+        private void startStopButton_Click(object sender, EventArgs e)
+        {
+            if(timer.Enabled)
+            {
+                timer.Enabled = false;
+                startStopButton.Text = "Start (Space)";
+            }
+            else if(!timer.Enabled)
+            {
+                timer.Enabled = true;
+                startStopButton.Text = "Stop (Space) ";
+            }
+        }
+
+        private void radiusTrackBar_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "bmp files (*.bmp)|*.bmp";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                    pictureBox1.Image.Save(saveFileDialog1.FileName);
+                                                                                                                         
+            }
+
+        }
+    }
+}
